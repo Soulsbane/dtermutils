a94df7f3082ede793194e0d529ecd6a02decfcb0
Paul Crane <paul@acheronsoft.com>
2018-02-13 22:57:28 -0600

    Add module inputcollector.

---
+++ source/dtermutils/inputcollector.d
@@ -0,0 +1,1 @@
+module dtermutils.inputcollector;
--- source/dtermutils/package.d
+++ source/dtermutils/package.d
@@ -3,6 +3,7 @@ module dtermutils;
 public
 {
 	import dtermutils.cursor;
+	import dtermutils.inputcollector;
 	import dtermutils.progressbar;
 	import dtermutils.prompts;
 }
